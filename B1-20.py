bff6902eb659f5ec871809a0adcec050dec11ef1
anvarbek8 <anvarraxmatov2006@gmail.com>
2024-10-26 15:22:37 +0500

    Add files via upload
    homework 1

---
+++ B1-20.py
@@ -0,0 +1,129 @@
+import math
+#B1
+# a = float(input("a = "))
+# print("P =", 4 * a)
+
+#B2
+# a = float(input("a = "))
+# print("S =", a**2)
+
+#B3
+#a = float(input("a = "))
+#b = float(input("b = "))
+#print("P =", 2 * (a + b))
+#print("S =", a * b)
+
+#B4
+#d = float(input("d = "))
+#pi = 3.14
+#print("L =", pi * d)
+
+#B5
+#a = float(input("a = "))
+#print("V =", a  3)
+#print("S =", 6 * a  2)
+
+#B6
+#a = float(input("a = "))
+#b = float(input("b = "))
+#c = float(input("c = "))
+#print("V = ", a * b * c)
+#print("S = ", 2 * (a * b + b * c + c *a))
+
+#B7
+#R = float(input("R = "))
+#pi = 3.14
+#print("L = ", 2 * pi * R)
+#print("S = ", pi * R  2)
+
+#B8
+#a = float(input("a = "))
+#b = float(input("b = "))
+#print("arithmetic mean = ", (a + b)/2)
+
+#B9
+#a = float(input("Enter non-negative a: "))
+#b = float(input("Enter non-negative b: "))
+#print("geometric mean = ", math.sqrt(a * b))
+
+#B10
+#a = float(input("Enter non-zero a: "))
+#b = float(input("Enter non-zero b: "))
+#print("a + b = ", a + b)
+#print("a * b = ", a * b)
+#print("a^2 = ", a  2)
+#print("b^2 = ", b  2)
+
+#B11
+#a = float(input("Enter non-zero a: "))
+#b = float(input("Enter non-zero b: "))
+#print("a + b = ", a + b)
+#print("a * b = ", a * b)
+#print("|a| = ", math.sqrt(a  2))
+#print("|b| = ", math.sqrt(b  2))
+
+#B12
+#a = float(input("a = "))
+#b = float(input("b = "))
+#c = math.sqrt(a  2 + b  2)
+#print("c = ", c)
+#print("P = ", a + b + c)
+
+#B13
+#R1 = float(input("R1 = "))
+#R2 = float(input("R2 = "))
+#pi = 3.14
+#print("R3 = ", pi * math.sqrt((R1 - R2)  2))4
+
+#B14
+#L = float(input("L = ")) 
+#pi = 3.14
+#R = L/(2 * pi)
+#print("R = ", R)
+#print("S = ", pi * R  2)
+
+#B15
+#S = float(input("S = "))
+#pi = 3.14
+#R = math.sqrt(S/pi)
+#print("d = ", 2 * R)
+#print("R = ", R)
+
+#B16
+#x1 = float(input("x1 = "))
+#x2 = float(input("x2 = "))
+#print("|x1 - x2| = ", ((x1 - x2)  2)  0.5)
+
+#B17
+#A = float(input("A = "))
+#B = float(input("B = "))
+#C = float(input("C = "))
+#AB = ((A - B)  2)  0.5
+#BC = ((B - C)  2)  0.5
+#CA = ((C - A)  2)  0.5
+#print("AC = ", CA)
+#print("BC = ", BC)
+#print("Total = ", AB + BC + CA)
+
+#B18
+#A = float(input("A = "))
+#B = float(input("B = "))
+#C = float(input("C = "))
+#BC = ((B - C)  2)  0.5
+#CA = ((C - A)  2)  0.5
+#print("AC * BC = ", BC * CA)
+
+#B19
+#x1 = float(input("x1 = "))
+#y1 = float(input("y1 = "))
+#x2 = float(input("x2 = "))
+#y2 = float(input("y2 = "))
+#print("P = ", 2 * ((((x1 - x2)  2)  0.5) + (((y1 - y2)  2)  0.5)))
+#print("S = ", (((x1 -x2) * (y1 - y2))  2)  0.5)
+
+#B20
+#x1 = float(input("x1 = "))
+#y1 = float(input("y1 = "))
+#x2 = float(input("x2 = "))
+#y2 = float(input("y2 = "))
+#print("distance = ", ((x1 - x2)  2 + (y1 - y2)  2) ** 0.5)
