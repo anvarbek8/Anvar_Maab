d8be517f046c1ece98495ab30ea93999dc061527
anvarbek8 <anvarraxmatov2006@gmail.com>
2024-10-26 15:23:36 +0500

    Add files via upload
    hw 1

---
+++ B21-40.py
@@ -0,0 +1,127 @@
+#B21
+# x1 = float(input("x1 = "))
+# y1 = float(input("y1 = "))
+# x2 = float(input("x2 = "))
+# y2 = float(input("y2 = "))
+# x3 = float(input("x3 = "))
+# y3 = float(input("y3 = "))
+# a = ((x1 - x2)**2 + (y1 - y2)**2)**0.5
+# b = ((x2 - x3)**2 + (y2 - y3)**2)**0.5
+# c = ((x3 - x1)**2 + (y3 - y1)**2)**0.5
+# P = a + b + c
+# print("P =", P)
+# print("S =", (P/2 * (P/2-a) * (P/2-b) * (P/2-c))**0.5)
+
+#B22
+#A = float(input("A = "))
+#B = float(input("B = "))
+#print("A =", B)
+#print("B =", A)
+
+#B23
+#A = float(input("A = "))
+#B = float(input("B = "))
+#C = float(input("C = "))
+#print("A =", C)
+#print("B =", A)
+#print("C =", B)
+
+#B24
+#A = float(input("A = "))
+#B = float(input("B = "))
+#C = float(input("C = "))
+#print("A =", B)
+#print("B =", C)
+#print("C =", A)
+
+#B25
+#x = float(input("x = "))
+#print("ans =", 3 * x**6 - 6 * x**2 - 7)
+
+#B26
+#x = float(input("x = "))
+#print("ans =", 4 * (x - 3)**6 - 7 * (x - 3)**3 + 2)
+
+#B27
+#A = float(input("A = "))
+#print("A^2 =", A**2)
+#print("A^4 =", A**4)
+#print("A^8 =", A**8)
+
+#B28
+#A = float(input("A = "))
+#print("A^2 =", A**2)
+#print("A^3 =", A**3)
+#print("A^5 =", A**5)
+#print("A^10 =", A**10)
+#print("A^15 =", A**15)
+
+#B29
+#alpha = float(input("alpha = "))
+#print("Radian =", alpha * math.pi/180)
+
+#B30
+#radian = float(input("radian = "))
+#print("alpha =", radian * 180/math.pi)
+
+#B31
+#temp = float(input("Temperature Farengeyt: "))
+#print("Temperature gradus selsey =", (temp - 32) * 5/9)
+
+#B33
+#X = float(input("X = "))
+#A = float(input("A = "))
+#Y = float(input("Y = "))
+#print("1 kg konfet", A/X, "so'm turadi")
+#print(Y, "kg konfet", A/X * Y, "so'm turadi")
+
+#B34
+#X = float(input("X = "))
+#A = float(input("A = "))
+#Y = float(input("Y = "))
+#B = float(input("B = "))
+#print("1 kg shokolad 1 kg konfetdan", A/X - B/Y, "so'm qimmat turadi")
+
+#B35
+#V = float(input("V = "))
+#U = float(input("U = "))
+#T1 = float(input("T1 = "))
+#T2 = float(input("T2 = "))
+#print("S =", T1 * (V + U) + T2 * (V - U))
+
+#B36
+#V1 = float(input("V1 = "))
+#V2 = float(input("V2 = "))
+#S = float(input("S = "))
+#T = float(input("T = "))
+#print("distance =", S + (V1 + V2) * T)
+
+#B37
+#V1 = float(input("V1 = "))
+#V2 = float(input("V2 = "))
+#S = float(input("S = "))
+#T = float(input("T = "))
+#print("distance =", S - (V1 + V2) * T)
+
+#B38
+#A = float(input("Enter non-zero A = "))
+#B = float(input("B = "))
+#print("x =", -B/A)
+
+#B39
+#A = float(input("Enter non-zero A = "))
+#B = float(input("B = "))
+#C = float(input("C = "))
+#D = B  2 - 4 * A * C
+#print("x1 =", (-B + D**0.5)/(2 * A))
+#print("x2 =", (-B - D ** 0.5)/(2 * A))
+
+#B40
+#A1 = float(input("A1 = "))
+#B1 = float(input("A1 = "))
+#C1 = float(input("A1 = "))
+#A2 = float(input("A1 = "))
+#B2 = float(input("A1 = "))
+#C2 = float(input("A1 = "))
+#print("x =", (C1 * B2 - C2 * B1)/(A1 * B2 - A2 * B1))
+#print("y =", (A1 * C2 - A2 * C1)/(A1 * B2 - A2 * B1))
